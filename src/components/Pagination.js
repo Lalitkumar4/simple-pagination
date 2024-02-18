5fe41ee1d7921cee0b0dea2f6bfc7f283ded9188
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2024-02-18 17:06:56 +0530

    add disable and active functionality on pagination bar

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,23 +1,56 @@
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
-  const pageNumbers = [];
+const Pagination = ({ postsPerPage, totalPosts, currentPage, paginate }) => {
+  const pageNumbers = []
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
+    pageNumbers.push(i)
   }
 
   return (
     <nav>
       <ul className="pagination">
-        {pageNumbers.map((number) => (
-          <li key={number} className="page-item">
-            <a onClick={() => paginate(number)} href="!#" className="page-link">
-              {number}
-            </a>
-          </li>
-        ))}
+        {pageNumbers.map((number) => {
+          if (number === 1 && currentPage === 1) {
+            return (
+              <li key={number} className="page-item disabled">
+                <a href="!#" className="page-link">
+                  1
+                </a>
+              </li>
+            )
+          }
+
+          if (
+            number === pageNumbers.length &&
+            currentPage === pageNumbers.length
+          ) {
+            return (
+              <li key={number} className="page-item disabled">
+                <a href="!#" className="page-link">
+                  {pageNumbers.length}
+                </a>
+              </li>
+            )
+          }
+
+          return (
+            <li
+              key={number}
+              className={`page-item ${currentPage === number ? "active" : ""}`}
+              aria-current="page"
+            >
+              <a
+                onClick={() => paginate(number)}
+                href="!#"
+                className="page-link"
+              >
+                {number}
+              </a>
+            </li>
+          )
+        })}
       </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default Pagination;
+export default Pagination
